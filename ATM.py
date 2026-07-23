ba844275779b91c027725d57bc13094db61ee603
A-liver <alijikiri153@gmail.com>
2026-07-24 07:59:08 +0800

    #

--- ATM.py
+++ ATM.py
@@ -9,9 +9,3 @@ withdrawal_amount = float(input("Enter the amount of your withdrawal:"))
 # Validation logic uses withdrawal_amount less than equal to account_balance
 # account_balance is deducted from the withdrawal_amount
 # else withdrawal amount is greater than account_balance
-if withdrawal_amount <= account_balance:
-    account_balance -= withdrawal_amount
-    print("Withdrawal successful!")
-    print(f"Remaining Balance: ₱{account_balance:.2f}")
-else:
-    print("Insufficient funds!")
