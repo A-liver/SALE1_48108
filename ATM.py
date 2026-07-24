c7d7b70119335df2f869c71d32bd9e7eddc53112
RogI318 <rogielynamisola27@gmail.com>
2026-07-25 01:58:13 +0800

    #

--- ATM.py
+++ ATM.py
@@ -1,5 +1,5 @@
 # ========Group Members====================
-# Rogielyn C. Amisola =
+# Rogielyn C. Amisola = 2411600094
 # Ali Akbhar C. Jikiri = 2411600058
 # Arjun B Lawag =
 # ========Details=========================
@@ -18,9 +18,16 @@ account_balance = 500.00
 # Welcome Message
 print ("Welcome to AJL ATM")
 # Withdrawal message withdrawal_amount equal to user input
-# code diri
+withdrawal_amount = float(input("Enter withdrawal amount: "))
+
 
 # Validation logic uses withdrawal_amount less than equal to account_balance
 # account_balance is deducted from the withdrawal_amount
 # else withdrawal amount is greater than account_balance
 # code diri
+
+if withdrawal_amount <= account_balance:
+    account_balance -= withdrawal_amount
+    print(f"Withdrawal successful. Remaining balance: ${account_balance:.2f}")
+else:
+    print("Insufficient funds.")
