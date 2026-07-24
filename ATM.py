5f2da166a80d4570cb8c042306066eba9c3caf07
A-liver <alijikiri153@gmail.com>
2026-07-24 08:17:49 +0800

    #

--- ATM.py
+++ ATM.py
@@ -1,11 +1,26 @@
+# ========Group Members====================
+# Rogielyn C. Amisola =
+# Ali Akbhar C. Jikiri = 2411600058
+# Arjun B Lawag =
+# ========Details=========================
+# Programming Language: Python
+# Description: This program simulates a basic
+# ATM Withdrawal System. It initializes an
+# account balance, asks the user for a
+# withdrawal amount, validates the transaction,
+# and displays the result.
+# ==========================================
+
+
 # Account balance account_balance = 500.00
 account_balance = 500.00
 
 # Welcome Message
-print ("Welcome to JAL ATM")
+print ("Welcome to AJL ATM")
 # Withdrawal message withdrawal_amount equal to user input
-withdrawal_amount = float(input("Enter the amount of your withdrawal:"))
+# code diri
 
 # Validation logic uses withdrawal_amount less than equal to account_balance
 # account_balance is deducted from the withdrawal_amount
 # else withdrawal amount is greater than account_balance
+# code diri
